6a7f23dc5e005af2065a38412108628a6ca711d9
Hamish Friedlander <hafriedlander@gmail.com>
2022-11-07 19:43:01 +1300

    Include __index__ files, one is needed

---
+++ sdgrpcserver/pipeline/text_embedding/__init__.py
@@ -0,0 +1,5 @@
+
+from .text_embedding import TextEmbedding
+from .basic_text_embedding import BasicTextEmbedding
+from .lpw_text_embedding import LPWTextEmbedding
+from .structured_text_embedding import StructuredTextEmbedding, KeyValueTensors
